d000bc423f4c757e5ffd304fadbd5a6acaf3cff5
Thomas Le Duc <dev@tleduc.de>
2021-12-16 17:55:45 +0100

    Add Courses page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,14 @@
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
+    if (route === "/courses") {
+      return <CoursesPage />;
+    }
     if (route === "/about") {
       return <AboutPage />;
     }
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <nav>
-      <a href="/">Home</a> | <a href="/about">About</a>
+      <a href="/">Home</a> | <a href="/courses">Courses</a> | <a href="/about">About</a>
     </nav>
   );
 }
