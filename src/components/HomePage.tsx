70c5dc6d0ae6137983d69f8970026b0de36e1eec
Thomas Le Duc <dev@tleduc.de>
2021-12-16 08:03:19 +0100

    Add first component "HomePage"

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div className="bg-light p-5 rounded-lg">
+      <h1>Pluralsight Administration </h1>
+      <p>React, Flux and React Router in ES6 for ultra-responsive web apps.</p>
+    </div>
+  );
+}
+
+export default HomePage;
