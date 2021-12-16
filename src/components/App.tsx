572bf5df6c3ab1ca573bcbe60a23f73db6ccca97
Thomas Le Duc <dev@tleduc.de>
2021-12-16 17:49:43 +0100

    Add header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,22 @@
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
 
 function App() {
-  const route = window.location.pathname;
-  if (route === "/about") {
-    return <AboutPage />;
+  function getPage() {
+    const route = window.location.pathname;
+    if (route === "/about") {
+      return <AboutPage />;
+    }
+    return <HomePage />;
   }
-  return <HomePage />;
+
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <nav>
+      <a href="/">Home</a> | <a href="/about">About</a>
+    </nav>
+  );
+}
+
+export default Header;
