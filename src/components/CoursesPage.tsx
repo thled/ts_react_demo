11c594a598a1f0937451ae524601cb33cf5a574d
Thomas Le Duc <dev@tleduc.de>
2021-12-17 07:46:26 +0100

    Show courses

--- src/components/CoursesPage.tsx
+++ src/components/CoursesPage.tsx
@@ -1,8 +1,58 @@
 import React from "react";
+import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
+type Course = {
+  id: number;
+  title: string;
+  authorId: number;
+  category: string;
+};
+
+type CoursesPageProps = {
+};
+
+type CoursesPageState = {
+  courses: Course[];
+};
+
+class CoursesPage extends React.Component<CoursesPageProps, CoursesPageState> {
+  state = {
+    courses: []
+  };
+
+  componentDidMount() {
+    getCourses()
+      .then((courses: Course[]) => {
+        this.setState({ courses: courses });
+      });
+  }
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <>
+        <h2>Courses</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author ID</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.state.courses.map( (course: Course) => {
+              return (
+                <tr key={course.id}>
+                  <td>{course.title}</td>
+                  <td>{course.authorId}</td>
+                  <td>{course.category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
   };
 }
 
